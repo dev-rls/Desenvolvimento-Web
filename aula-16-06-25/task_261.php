0b0b433056f04327e91d711ce48782b72ebb8faa
Raquel Santos <181283602+dev-rls@users.noreply.github.com>
2025-06-16 16:56:06 -0400

    feat: create btn
    btn voltar

---
+++ aula-16-06-25/task_261.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LandGrow - Business Intelligence</title>
+    <link rel="stylesheet" href="../../public/css/adm.css">
+</head>
+<body>
+    <div class="btn_voltar">
+        <a href="#"><img class="img_sair_tela_editar_parceiros" src="../../public/imagens/bnt_voltar.png" alt="Botão Voltar"></a>
+    </div>
+</body>
+</html>
